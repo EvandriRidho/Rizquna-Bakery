b4dbf95ab36a6f98856490968a479ff708ce221e
unknown <evandriridho555@gmail.com>
2025-05-15 19:10:54 +0700

    feat: ✨ add category section to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner"
+import Categories from "../components/categories"
 
 const Home = () => {
     return (
         <div className="mt-10">
             <Banner />
+            <Categories />
         </div>
     )
 }
