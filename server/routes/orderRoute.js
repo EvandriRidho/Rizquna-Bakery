cc063ba2fdf327cef9db971035dd2db704a65d04
EvandriRidho <evandriridho555@gmail.com>
2025-06-18 18:52:47 +0700

    feat (Server) : Build API for Order

---
+++ server/routes/orderRoute.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { getAllOrders, getUserOrders, placeOrderCOD } from '../controllers/orderController.js'
+import authUser from '../middlewares/authUser.js'
+import authSeller from '../middlewares/authSeller.js'
+
+const orderRouter = express.Router()
+
+orderRouter.post('/cod', authUser, placeOrderCOD)
+orderRouter.post('/user', authUser, getUserOrders)
+orderRouter.post('/seller', authSeller, getAllOrders)
+
+export default orderRouter
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import productRouter from './routes/productRoute.js';
 import connectCloudinary from './configs/cloudinary.js';
 import cartRouter from './routes/cartRoute.js';
 import addressRouter from './routes/addressRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -30,6 +31,7 @@ app.use('/api/seller', sellerRouter)
 app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
 app.use('/api/address', addressRouter)
+app.use('/api/order', orderRouter)
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
