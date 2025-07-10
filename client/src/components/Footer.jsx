4bccf1fd2b5e5c78ba70c3638a0e87be0acfd6f7
EvandriRidho <evandriridho555@gmail.com>
2025-07-11 00:19:52 +0700

    style (client) : redesign footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
                     </div>
                     <p className="max-w-[410px] mt-6">Rizquna Bakery menghadirkan roti dan kue segar setiap hari dengan kualitas terbaik, siap diantar langsung ke rumah Anda dengan cepat dan mudah.</p>
                 </div>
-                <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+                {/* <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
                     {footerLinks.map((section, index) => (
                         <div key={index}>
                             <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">{section.title}</h3>
@@ -26,7 +26,7 @@ const Footer = () => {
                             </ul>
                         </div>
                     ))}
-                </div>
+                </div> */}
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
                 Copyright {new Date().getFullYear()} © Rizquna Bakery All Right Reserved.
