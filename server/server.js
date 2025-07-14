a28c99e14c25b46c0ae4062cb01214f78b13f9eb
EvandriRidho <evandriridho555@gmail.com>
2025-07-14 22:27:28 +0700

    testing midtrans

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use('/api/order', orderRouter)
 
 // Server
 app.get('/', (req, res) => {
-    res.send('Rizquna Bakery API!! Working...')
+    res.send('Rizquna Bakery API!! Working... in Production')
 })
 
 app.listen(PORT, () => {
